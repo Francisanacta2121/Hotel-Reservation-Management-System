1ae6e937e86b9cfcd63521abcfcbca54e8c45fbd
Francisanacta2121 <anactafrancisrico@gmail.com>
2025-10-23 14:31:47 +0800

    Delete hrms3.0 directory

--- hrms3.0/inc/flash.php
+++
@@ -1,7 +0,0 @@
-<?php
-function flash($key){
-  if(!isset($_SESSION[$key])) return "";
-  $m = $_SESSION[$key]; unset($_SESSION[$key]);
-  return '<div class="alert">'.esc($m).'</div>';
-}
-?>
--- hrms3.0/inc/sessiontest.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once __DIR__ . "/../inc/config.php";
-echo "Session is fine!";
